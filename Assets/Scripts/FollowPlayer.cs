8e13bd0e2d7e5d56bf75996eb114eab1af8e1cbf
emesefe <maria@frogames.es>
2024-02-05 07:57:49 +0100

    Repaso del código y comentarios

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -1,17 +1,18 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.PlayerLoop;
 
 public class FollowPlayer : MonoBehaviour
 {
-    [SerializeField] private GameObject player;
-    [SerializeField] private Vector3 offset = new Vector3(0, 5, -10);
+    [SerializeField] private GameObject player; // Asignamos la referencia desde el inspector
+    
+    private Vector3 offset = new Vector3(0, 5, -10);
 
     private void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
-        transform.rotation = player.transform.rotation;
+        /* Cambiamos la posición de la cámara en el LateUpdate para que primero se mueva el player y luego,
+            en función a ese movimiento, se mueva la cámara para perseguirlo. De este modo evitamos que la 
+            cámara vaya a trompicones
+         */
+        transform.position = player.transform.position + offset; // La cámara modifica su posición para seguir al player
+        transform.rotation = player.transform.rotation; // Cámara modifica su rotación para mirar al player
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,16 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    //[SerializeField] private GameObject camera;
-    
-    [SerializeField] private float speed = 10.0f;
-    [SerializeField] private float lateralSpeed = 5f;
+    private float speed = 15.0f; // Velocidad del vehículo
+    private float lateralSpeed = 5f; // Velocidad de rotación
 
-    //[SerializeField] private Vector3 offset = new Vector3(0, 5, -10);
-    
     private float horizontalInput;
     private float verticalInput;
 
@@ -20,28 +14,26 @@ public class Movement : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
 
-        // Recomendación: si trabajamos con direcciones, que sean vectores unitarios
-        // Vector3.right se corresponde con (1, 0, 0)
-        // Vector3.left se corresponde con (-1, 0, 0)
-        // Vector3.up se corresponde con (0, 1, 0)
-        // Vector3.down se corresponde con (0, -1, 0)
-        // Vector3.forward se corresponde con (0, 0, 1)
-        // Vector3.back se corresponde con (0, 0, -1)
+        /* Recomendación: si trabajamos con direcciones, que sean vectores unitarios (su módulo es 1)
+            * Vector3.right se corresponde con (1, 0, 0)
+            * Vector3.left se corresponde con (-1, 0, 0)
+            * Vector3.up se corresponde con (0, 1, 0)
+            * Vector3.down se corresponde con (0, -1, 0)
+            * Vector3.forward se corresponde con (0, 0, 1)
+            * Vector3.back se corresponde con (0, 0, -1)
+        */
         
-        // Movimiento en función del sistema de coordenadas local (el del objeto)
-        // Movimiento hacia adelante automático
+        // Usando transform.Translate, el movimiento es en función del sistema de coordenadas local (el del objeto)
+        // Movimiento hacia adelante automático (no depende del input del usuario)
         // transform.Translate(Vector3.forward * speed * Time.deltaTime);
         
-        // Movimiento hacia adelante afectado por el input del usuario (vertical)
+        // Movimiento hacia adelante afectado por el input vertical del usuario 
         transform.Translate(Vector3.forward * speed * Time.deltaTime * verticalInput);
         
-        // Movimiento lateral afectado por el input del usuario (horizontal)
-        //transform.Translate(Vector3.right * lateralSpeed * Time.deltaTime * horizontalInput);
+        // Movimiento lateral afectado por el input horizontal del usuario
+        // transform.Translate(Vector3.right * lateralSpeed * Time.deltaTime * horizontalInput);
         
+        // Rotación afectada por el input horizontal del usuario
         transform.Rotate(Vector3.up, lateralSpeed * Time.deltaTime * horizontalInput);
-        
-        
-
-        // camera.transform.position = transform.position + offset;
     }
 }
